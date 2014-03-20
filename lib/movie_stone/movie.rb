9582f2b68ad98b97a156273a61247f4911cc1bcc
Adrian Perez <adrianperez.deb@gmail.com>
2014-03-20 18:32:59 +0100

    Cast movie rating and year

--- lib/movie_stone/movie.rb
+++ lib/movie_stone/movie.rb
@@ -4,9 +4,9 @@ require 'hashie/trash'
 module MovieStone
   class Movie < Hashie::Trash
     property :title,        from: :Title
-    property :rating,       from: :imdbRating
+    property :rating,       from: :imdbRating, with: ->(i) { i.to_f }
     property :release_date, from: :Release
-    property :year,         from: :Year
+    property :year,         from: :Year, with: ->(i) { i.to_i }
     property :genre,        from: :Genre
     property :actors,       from: :Actors
     property :summary,      from: :Plot
